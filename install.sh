8b0b02547683b2d23731f427ef0d0956da3b884e
Dom <121951646+m0thman70@users.noreply.github.com>
2024-10-15 00:51:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 target=target/release/atto
 bin=/usr/bin
 
-cargo build --release
-chmod +x $target
-mv $target $bin
+if [ $(id -u) == 0 ]; then
+  cargo build --release
+  chmod +x $target
+  mv $target $bin
+else
+  echo "install.sh must be run with sudo. Doing nothing."
+fi
